0a74dbcf0ac44847addc174e50be3d343e98407a
Kaitoukid93 <saorihara93@gmail.com>
2024-10-08 02:05:55 +0700

    Update App Icon

--- MockupImageProccessing/obj/Debug/net8.0/MockupImageProccessing.AssemblyInfo.cs
+++ MockupImageProccessing/obj/Debug/net8.0/MockupImageProccessing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MockupImageProccessing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed8cb03930cb61ee30f2c10a2ef69f5e337256c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3d6a8a5400064a846a4836abb0a399cef1ceb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MockupImageProccessing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MockupImageProccessing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
